a86082e20bbf5bfe4e4d19c5fc1d34d4126c5880
Gowtham Kumar Rodda <gowtham.kumar@legatohealth.com>
2022-12-17 12:46:58 +0530

    added employee class

---
+++ src/main/java/payroll/Employee.java
@@ -0,0 +1,65 @@
+package payroll;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "name")
+    private String name;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (super.equals(obj))
+            return true;
+        if (!(obj instanceof Employee))
+            return false;
+        Employee employee = (Employee) obj;
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.role, employee.role) &&
+                Objects.equals(this.name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.role);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
